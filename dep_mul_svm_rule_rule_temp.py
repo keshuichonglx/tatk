06f0321c8201159fa1ce5568870dd3add824b3ed
lixiang <lixiang890217@163.com>
2019-07-10 20:23:57 +0800

    add cam_rule_rule_temp, not completed

--- dep_mul_svm_rule_rule_temp.py
+++ dep_mul_svm_rule_rule_temp.py
@@ -25,6 +25,7 @@ from DeployClient import DeployClient, FunctionRunError, MyLock
 # 这里设置服务启动相关设置,port=开放的端口,max_items=最大缓存数据条数,expire_sec=缓存超时时间(秒)
 app = DeployClient(module_name=__name__, port=7777, max_items=1000, expire_sec=600)
 
+'''
 # 这里进行模型的实例化或一些模型必要的加载和初始化工作
 nlu = SVMNLU('usr', model_file=os.path.join(root_dir, 'res/svm_multiwoz_usr.zip'))
 dst = RuleDST()
@@ -58,6 +59,41 @@ def inference(input: dict, buffer: dict) -> (dict, dict):
         raise FunctionRunError('running error')
     finally:
         model_lock.leave()
+
+    print('output state:' + str(new_buffer))
+    # back
+    output = {'reqt': reqt}
+
+    return output, new_buffer
+'''
+# 这里进行模型的实例化或一些模型必要的加载和初始化工作
+nlu = SVMNLU('usr', model_file=os.path.join(root_dir, 'res/svm_multiwoz_usr.zip'))
+policy = Rule()
+nlg = TemplateNLG(is_user=False)
+state_ini = copy.deepcopy(RuleDST().state)
+
+
+@app.inference_buffer('multiwoz_svm_rule_rule_temp')
+def inference(input: dict, buffer: dict) -> (dict, dict):
+    # 这里通过input作为输入，计算得到output结果
+    if 'post' not in input.keys():
+        raise FunctionRunError('Missing argument in input')
+
+    # 如果是第一轮，那么给一个初始默认的空dict
+    if not buffer:
+        print('ini new state')
+        buffer = copy.deepcopy(state_ini)
+    print('input state :' + str(buffer))
+
+    # 开始执行
+    dst = RuleDST()
+    sys_agent = PipelineAgent(nlu, dst, policy, nlg)
+    try:
+        sys_agent.tracker.state = buffer
+        reqt = sys_agent.response(input['post'])
+        new_buffer = copy.deepcopy(sys_agent.tracker.state)
+    except Exception:
+        raise FunctionRunError('running error')
 
     print('output state:' + str(new_buffer))
     # back
